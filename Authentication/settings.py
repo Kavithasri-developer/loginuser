af9c62673f1b6f3f9e4398f8a150f821bb427da5
kavithasri <kavithakavithasri101@gmail.com>
2025-03-13 19:17:36 +0530

    Added gunicorn for deployment

--- Authentication/settings.py
+++ Authentication/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+from django.core.wsgi import get_wsgi_application
 from pathlib import Path
 
 
@@ -21,12 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-++c=!y&g2h)87cvka@2tr_ykh0!&i2kb0itj(=lz2rk!iv%sw%'
+# SECRET_KEY = 'django-insecure-++c=!y&g2h)87cvka@2tr_ykh0!&i2kb0itj(=lz2rk!iv%sw%'
+SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = ['your-app-name.onrender.com', 'localhost']
+
 
 
 # Application definition
@@ -120,7 +126,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATICFILES_DIRS = [BASE_DIR,"static"]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
 # Default primary key field type
